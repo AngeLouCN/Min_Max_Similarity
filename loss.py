cff0f965914181386346a92c833ad35987f92394
AngeLouCN <79243162+AngeLouCN@users.noreply.github.com>
2022-10-04 11:04:30 -0500

    Add files via upload

--- loss.py
+++ loss.py
@@ -12,6 +12,7 @@ def weighted_loss(pred, mask):
     wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
     wbce = (weit*wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
 
+    # pred = torch.sigmoid(pred)
     inter = ((pred * mask)*weit).sum(dim=(2, 3))
     union = ((pred + mask)*weit).sum(dim=(2, 3))
     wiou = 1 - (inter + 1)/(union - inter+1)
@@ -20,18 +21,12 @@ def weighted_loss(pred, mask):
 
 
 
-
-    
-# labels for adversarial training
-
-
-
 def calc_loss(pred, target, bce_weight=0.5):
     bce = weighted_loss(pred, target)
-    dl = 1 - dice_coef(pred, target)
-    loss = bce * bce_weight + dl * bce_weight
+    # dl = 1 - dice_coef(pred, target)
+    # loss = bce * bce_weight + dl * bce_weight
 
-    return loss
+    return bce
 
 
 def loss_sup(logit_S1, logit_S2, labels_S1, labels_S2):
@@ -41,12 +36,16 @@ def loss_sup(logit_S1, logit_S2, labels_S1, labels_S2):
     return loss1 + loss2
 
 
+
 def loss_diff(u_prediction_1, u_prediction_2, batch_size):
-    a = calc_loss(u_prediction_1, Variable(u_prediction_2, requires_grad=False))
+    a = weighted_loss(u_prediction_1, Variable(u_prediction_2, requires_grad=False))
     a = a.item()
 
-    b = calc_loss(u_prediction_2, Variable(u_prediction_1, requires_grad=False))
+    b = weighted_loss(u_prediction_2, Variable(u_prediction_1, requires_grad=False))
     b = b.item()
 
     loss_diff_avg = (a + b)
     return loss_diff_avg / batch_size
+
+
+
